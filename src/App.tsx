201e0f0a69d5d96f1e31abe7c120774b4da20b17
vmalaviya <varunmalaviya7@gmail.com>
2024-01-08 20:19:19 -0500

    initial components for website

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import './App.css'
+import { useState } from 'react'
+import NavBar from './components/NavBar'
+import Footer from './components/Footer'
+import Hero from './components/Hero'
 
 function App() {
-  return (
-    <>
-        <div className='text-3xl'>
-            Welcome to GoSpace
-        </div>
-    </>
-  )
+    //using state to identify the current page the user is browser
+    const [ currentPage, setCurrentPage ] = useState<string>('home')
+    
+    return (
+        <>
+            <NavBar 
+                page='N/A'
+                currentPage={currentPage}
+                setCurrentPage={setCurrentPage}
+            />
+            <Hero />
+            <Footer />
+        </>
+    )
 }
 
 export default App

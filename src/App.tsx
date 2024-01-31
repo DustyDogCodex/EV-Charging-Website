a5c3a204375595701e94acdf5c7ef9ef1c941c29
vmalaviya <varunmalaviya7@gmail.com>
2024-01-31 17:50:21 -0500

    inital commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+        <div className='text-3xl'>
+            Welcome to GoSpace
+        </div>
+    </>
+  )
+}
+
+export default App
